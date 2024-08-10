daf5a9964d3e31e3aed586a979d83706a5a0ff8d
Ali Al-Bayaty <albayaty@users.noreply.github.com>
2024-08-10 00:27:33 -0700

    Update cus.py

--- grover_controlled_diffuser/cus.py
+++ grover_controlled_diffuser/cus.py
@@ -4,21 +4,21 @@
 
 def CUs(quantum_circuit, inputs, output, barriers=False):
     """
-    This function creates our innovated Grover controlled-diffuser (CUs),
+    This function creates our innovative Grover controlled-diffuser (CUs),
     which is the standard Grover diffuser (Us) of Grover's algorithm that
     is controlled by the output qubit of a Boolean oracle. The CUs operator is 
     well-designed to search for all solutions for Boolean oracles only, where
     the standard Us operator fails to search for all solutions!
     
     Parameters
-	----------
+    ----------
     quantum_circuit: the quantum circuit of Grover's algorithm,
     inputs: the list of input qubits' indices of a Boolean oracle,
     output: the index of functional qubit (fqubit) of a Boolean oracle, and
     barriers: draw barriers (separators) around the CUs operator.
     
     Returns
-	-------
+    -------
     The quantum circuit of Grover's algorithm with the Grover controlled-diffuser (CUs).
     
     For more information, please check our paper available at
@@ -26,7 +26,7 @@ def CUs(quantum_circuit, inputs, output, barriers=False):
     """
     
     if barriers:
-        quantum_circuit.barrier()
+	    quantum_circuit.barrier()
     
     # The 2nd rotation:
     quantum_circuit.h( inputs )
@@ -46,6 +46,6 @@ def CUs(quantum_circuit, inputs, output, barriers=False):
     quantum_circuit.h( inputs )
     
     if barriers:
-        quantum_circuit.barrier()
+	    quantum_circuit.barrier()
     
     return quantum_circuit
